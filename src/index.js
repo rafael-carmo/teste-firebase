f7b1dbf3526c7a9778f2dc52c6d2837607acbcd1
rafael <rafaelljj@gmail.com>
2022-03-02 15:05:27 -0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Paperbase from './Paperbase';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Paperbase />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
